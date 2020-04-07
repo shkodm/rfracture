d0e2aefbab3e64bcebe123c6fa41b107915b1687
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2020-04-08 09:30:28 +1000

    update of stl

--- examples/stls.R
+++ examples/stls.R
@@ -46,24 +46,28 @@ ret2$gap*G*32
 
 
 alpha = 4.5
-power.iso = function(f) 0.000001*ifelse(f<5,1,(f/5)^-alpha)
+sd = 0.001
+#sd = 0.000
+power.iso = function(f) sd^2*ifelse(f<5,1,(f/5)^-alpha)
 corr.profile = function(lambda) ifelse(lambda<0.5,0.1,0.9)
-corr.profile = function(lambda) 0.9
+corr.profile = function(lambda) 0.2
 
 G = 12
 mar = 1/G/32 * 0.6
 
-gap = 16
-gap = gap / (G*32)
 refine = 32
+gap_n = 16
 for (refine in 2^{1:6}){
   ret = fracture_geom(refine=refine, corr.profile = corr.profile, power.iso = power.iso, seed=123)
-  for (closed in seq(0.05,0.95,0.05)){
+  for (gap_n in 0:16){
+    gap = gap_n / (G*32)
     ret2 = set_gap(ret, gap = gap)
     ret2 = slice(ret2,  value="above")
     ret2 = slice(ret2,  level=1/G - 2*mar, flatten="above")
     if (require(rgl)) fracture3d(cut(ret2),edge.col = 4, vertex.col = 4)
     print(cbind(range(ret2$points$f1, ret2$points$f2), c(mar,1/G-mar), c(0,1/G)))
-    write.stl(cut(ret2), sprintf("seam_test_G%02d_R%02d.stl",round(closed*100),refine))
+    write.stl(cut(ret2), sprintf("seam_test_G%02d_R%02d.stl",gap_n,refine))
   }
 }
+
+cov2cor(ret$cov.final)
