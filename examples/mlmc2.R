2c99f52e66cc5ebce386cce74dbe45cf0b025d0f
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2020-04-24 17:13:00 +1000

    mlmc update

--- examples/mlmc2.R
+++ examples/mlmc2.R
@@ -1,15 +1,23 @@
 library(Matrix)
 
-N = 300
-set.seed(124)
+N = 5000
+cost = c(1,2,3)
+#cost = c(1,1,1)
+set.seed(123)
 Ytable = matrix(rnorm(N*3),N,3,byrow = TRUE)
-Ytable = cbind(Ytable,1) %*% matrix(c(1/3,1/10,1,1,0,1/10,1,1.5,0,0,1,2),4,3)
+Ymat = matrix(c(1/3,1/13,1,1,0,1/10,1,1.5,0,0,1,2),4,3)
+#Ymat = matrix(c(1,0,0,1,0,1,0,1.5,0,0,1,2),4,3)
+Ytable = cbind(Ytable,1) %*% Ymat
+Ytable = cbind(Ytable,1) %*% matrix(c(1,0,0,1,0,1,0,1.5,0,0,1,2),4,3)
 
+realK = t(Ymat) %*% diag(c(1,1,1,0)) %*% Ymat
+
+#realK = diag(3)
 #rgl::plot3d(Ytable)
 
 levs = factor(paste0("lev",1:3))
 nlevs = nlevels(levs)
-x = expand.grid(seed = 1:4, lev = levs)
+x = expand.grid(lev = levs,seed = 1:20)
 
 fun = function(seed,lev) Ytable[seed+nrow(Ytable)*(as.integer(lev)-1)]
 
@@ -21,7 +29,7 @@ x_tab = reshape2::dcast(x, seed~lev, value.var = "val")
 
 sparseZero = function(n,m) sparseMatrix(i=integer(0),j=integer(0),x=numeric(0),dims=c(n,m))
 
-MyCov = function(x,y) {
+MyCov = function(x,y,BASE=K) {
   X = data.frame(seed=x$seed, lev=as.integer(x$lev), i=seq_len(nrow(x)))
   Y = data.frame(seed=y$seed, lev=as.integer(y$lev), i=seq_len(nrow(y)))
   
@@ -29,7 +37,7 @@ MyCov = function(x,y) {
   sparseMatrix(
     i=XY$i.x,
     j=XY$i.y,
-    x=K[XY$lev.x + (XY$lev.y-1)*nlevs],
+    x=BASE[XY$lev.x + (XY$lev.y-1)*nlevs],
     dims=c(nrow(X),nrow(Y)))
 }
 
@@ -44,19 +52,26 @@ MyH = function(x) {
 Z = sparseZero(3,3)
 
 K = diag(nrow=nlevs)
-
+log = NULL
+for (ad in seq_len(2000)) {
+  x = x[order(x$seed,x$lev),]
+  K = realK
+  #x_tab = reshape2::dcast(x, seed~lev, value.var = "val")
+  #K = cov(x_tab[,1:3+1],use="pairwise") 
 for (it in seq_len(50)) {
   xH = MyH(x)
   xS = MyCov(x,x)
   xM = rbind(cbind(xS,xH),cbind(t(xH),Z))
-  
+  MY = solve(xM,c(x$val,rep(0,nlevs)))
+  mu = MY[nrow(x)+1:3]
   xe = expand.grid(seed = unique(x$seed), lev = levs)
-  xe$val = cbind(MyCov(xe,x),MyH(xe)) %*% solve(xM,c(x$val,rep(0,nlevs)))
+  xe$val = cbind(MyCov(xe,x),MyH(xe)) %*% MY
   
   xe_tab = reshape2::dcast(xe, seed~lev, value.var = "val")
   K_ = cov(xe_tab[,seq_len(nlevs)+1])
   r = sqrt(sum((K-K_)^2))
-  print(r)
+  #print(r)
+  break;
   if (r < 1e-10) break;
   K = K_
 }
@@ -65,21 +80,55 @@ h = rep(0,nrow(x)+3)
 h[nrow(x)+3] = 1
 
 nx = expand.grid(seed = max(x$seed)+1, lev = levs)
-nx = rbind(nx,data.frame(seed=4,lev="lev3"))
+
+lev_seed = sapply(levs, function(l) max(x$seed[x$lev == l])) + 1
+nx = rbind(
+  expand.grid(seed = max(x$seed) + 1, lev=levs),
+  data.frame(seed = sapply(levs, function(l) max(x$seed[x$lev == l]))+1, lev = levs)
+)
+#nx = unique(nx)
+
 nS = MyCov(x,nx)
 nH = MyH(nx)
 b = rbind(nS,t(nH))
 g = solve(xM,b)
 c = diag(MyCov(nx,nx))
 
-vardif = (h %*% g)^2 / (c - colSums(g*b))
+mh = solve(xM,h)
+var = -as.vector(h %*% solve(xM,h))
+wei = as.vector(mh)[seq_len(nrow(x))]
+length(wei)
+var2 = as.vector(wei %*% MyCov(x,x,realK) %*% wei)
+vardif = as.vector((h %*% g)^2 / (c - colSums(g*b)))
 
-cost = c(1,2,3)
+vardifpercost = vardif / cost[nx$lev]
+log = rbind(log,c(sum(cost[x$lev]),var,var2,vardif,vardifpercost, mu))
+#print(vardif)
 
-vardif / cost[nx$lev]
-x
+sel = which.max(vardifpercost)
 
-sum(h*solve(xM,h))
-sum(c(h,0)*solve(cbind(rbind(xM,b[,3]),c(b[,3],c[3])),c(h,0)))
+add_x = nx[sel,]
+add_x$val = fun(add_x$seed,add_x$lev)
+print(add_x)
+x = rbind(x,add_x)
+if (nrow(log) %% 10 == 0) {
+  par(mfrow=c(2,2))
+  matplot(log[,1],log[,1:6+8],log="y",type="l",col=rep(1:3,times=2),lty=rep(1:2,each=3))
+#  plot(-log[,2],log="xy")
+#  abline(0,-1)
+  #plot(log[,1],log[,17]); abline(h=2)
+  #lines(log[,1],2 + sqrt(-log[,2]),lty=2)
+  #lines(log[,1],2 - sqrt(-log[,2]),lty=2)
+  matplot(log[,2:3],log="y",type="l",lty=1)
+  lines(realK[3,3]/seq_len(nrow(log)),lty=2)
+  barplot(table(x$lev))
+}
+}
 
+
+#plot(-log[,2],log="xy")
+#abline(0,-1)
+
+
+#x_tab = reshape2::dcast(x, seed~lev, value.var = "val")
 
