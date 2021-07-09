62c2d38808fec9ed1e43b8917331f724553f13e7
L Laniewski-Wollk <uqllanie@uq.edu.au>
2021-07-09 15:14:52 +1000

    moving the extract implementation to simplexes

--- R/extract.R
+++ R/extract.R
@@ -12,79 +12,54 @@ sortRows = function(M) {
 #' @export
 extract.tet.mesh = function(obj, type=c("top","bottom"), transform=function(points) points) {
   if (! "fracture_geom" %in% class(obj)) stop("'obj' not of fracture_geom class")
-  if (! is.character(type)) stop("'type' not a string")
+  if (! missing(type)) if (! is.character(type)) stop("'type' not a string")
   points = data.frame(
     x = c(obj$points[, "x"], obj$points[, "x"], obj$points[, "x"]),
     y = c(obj$points[, "y"], obj$points[, "y"], obj$points[, "y"]),
     z = c(obj$points[,"f1"], obj$points[,"f2"], obj$points[,"fm"])
   )
-  
   k = nrow(obj$points)
-  edges = NULL
-  triangles = NULL
-  tetrahedra = NULL
+  simplexes = NULL
+
+  add.simplexes = function(v1,v2=NA, v3=NA, v4=NA, tag) {
+    simplexes <<- rbind(simplexes, data.frame(v1=v1,v2=v2,v3=v3,v4=v4,tag=tag,stringsAsFactors=TRUE))
+  }
   
   ed = obj$edge
   tr = obj$triangles
   br = ifelse(obj$border, "border","edge")
-  edges = data.frame(
-    v1 = c(ed[,1],ed[,1]+k,ed[,1]+2*k),
-    v2 = c(ed[,2],ed[,2]+k,ed[,2]+2*k),
-    tag = c(paste("top",br),paste("bottom",br),paste("middle",br)),
-    stringsAsFactors = TRUE
-  )
   
+  add.simplexes( v1 = ed[,1],     v2 = ed[,2],      tag = paste("top",br))
+  add.simplexes( v1 = ed[,1]+k,   v2 = ed[,2]+k,    tag = paste("bottom",br))
+  add.simplexes( v1 = ed[,1]+2*k, v2 = ed[,2]+2*k,  tag = paste("middle",br))
   ed = sortRows(ed)
-  triangles = data.frame(
-    v1 = c(tr[,1], tr[,1]+k, tr[,1]+2*k, ed[,1], ed[,2]+k),
-    v2 = c(tr[,2], tr[,2]+k, tr[,2]+2*k, ed[,2], ed[,1]+k),
-    v3 = c(tr[,3], tr[,3]+k, tr[,3]+2*k, ed[,2]+k, ed[,1]),
-    tag = c(
-      rep("top",nrow(tr)),
-      rep("bottom",nrow(tr)),
-      rep("middle",nrow(tr)),
-      br,
-      br
-    ),
-    stringsAsFactors = TRUE
-  )
+  add.simplexes( v1 = tr[,1],     v2 = tr[,2],      v3 = tr[,3],      tag = "top")
+  add.simplexes( v1 = tr[,1]+k,   v2 = tr[,2]+k,    v3 = tr[,3]+k,    tag = "bottom")
+  add.simplexes( v1 = tr[,1]+2*k, v2 = tr[,2]+2*k,  v3 = tr[,3]+2*k,  tag = "middle")
+  add.simplexes( v1 = ed[,1],     v2 = ed[,2],      v3 = ed[,2]+k,    tag = br)
+  add.simplexes( v1 = ed[,2]+k,   v2 = ed[,1]+k,    v3 = ed[,1],      tag = br)
   tr = sortRows(tr)
-  tetrahedra = data.frame(
-    v1 = c(tr[,1]+k,tr[,2]+k,tr[,3]+k),
-    v2 = c(tr[,1]  ,tr[,2]  ,tr[,3]  ),
-    v3 = c(tr[,2]  ,tr[,3]  ,tr[,1]+k),
-    v4 = c(tr[,3]  ,tr[,1]+k,tr[,2]+k),
-    tag = "interior",
-    stringsAsFactors = TRUE
-  )
-  sel = abs(points$z[tetrahedra$v1] - points$z[tetrahedra$v2]) < 1e-10
-  tetrahedra = tetrahedra[!sel,,drop=FALSE]
-
-  edges = edges[edges$tag %in% type,,drop=FALSE]
-  triangles = triangles[triangles$tag %in% type,,drop=FALSE]
-  tetrahedra = tetrahedra[tetrahedra$tag %in% type,,drop=FALSE]
+  add.simplexes( v1 = tr[,1]+k,   v2 = tr[,1],      v3 = tr[,2],      v4 = tr[,3],     tag = "interior")
+  add.simplexes( v1 = tr[,2]+k,   v2 = tr[,2],      v3 = tr[,3],      v4 = tr[,1]+k,   tag = "interior")
+  add.simplexes( v1 = tr[,3]+k,   v2 = tr[,3],      v3 = tr[,1]+k,    v4 = tr[,2]+k,   tag = "interior")
   
-  selp = unique(c(
-    edges$v1, edges$v2,
-    triangles$v1, triangles$v2, triangles$v3,
-    tetrahedra$v1, tetrahedra$v2, tetrahedra$v3, tetrahedra$v4
-  ))
+  simplexes$order = ifelse(is.na(simplexes$v4),ifelse(is.na(simplexes$v3),ifelse(is.na(simplexes$v2),ifelse(is.na(simplexes$v1),0,1),2),3),4)
+  if (! missing(type)) simplexes = simplexes[simplexes$tag %in% type, , drop=FALSE]
+  selp = na.omit(unique(c(simplexes$v1,simplexes$v2,simplexes$v3,simplexes$v4)))
   map = rep(0,nrow(points)); map[selp] = seq_len(length(selp))
+  
   points = points[selp,,drop=FALSE]
-  edges$v1      = map[edges$v1]
-  edges$v2      = map[edges$v2]
-  triangles$v1  = map[triangles$v1]
-  triangles$v2  = map[triangles$v2]
-  triangles$v3  = map[triangles$v3]
-  tetrahedra$v1 = map[tetrahedra$v1]
-  tetrahedra$v2 = map[tetrahedra$v2]
-  tetrahedra$v3 = map[tetrahedra$v3]
-  tetrahedra$v4 = map[tetrahedra$v4]
+  simplexes$v1 = map[simplexes$v1]
+  simplexes$v2 = map[simplexes$v2]
+  simplexes$v3 = map[simplexes$v3]
+  simplexes$v4 = map[simplexes$v4]
+  
   points = transform(points)
   list(
-    points=points,
-    edges=edges,
-    triangles=triangles,
-    tetrahedra=tetrahedra
+    points     = points,
+    vertexes   = simplexes[simplexes$order == 1,,drop=FALSE],
+    edges      = simplexes[simplexes$order == 2,,drop=FALSE],
+    triangles  = simplexes[simplexes$order == 3,,drop=FALSE],
+    tetrahedra = simplexes[simplexes$order == 4,,drop=FALSE]
   )
 }
