81e2d33f0f5a4499c51fa349a7b68fb831305171
L Laniewski-Wollk <uqllanie@uq.edu.au>
2021-07-09 16:11:18 +1000

    adding option for genie fix

--- R/extract.R
+++ R/extract.R
@@ -10,7 +10,7 @@ sortRows = function(M) {
 #' @param tranform the function used for transform the points
 #' 
 #' @export
-extract.tet.mesh = function(obj, type=c("top","bottom"), transform=function(points) points) {
+extract.tet.mesh = function(obj, type=c("top","bottom"), transform=function(points) points, genie.h) {
   if (! "fracture_geom" %in% class(obj)) stop("'obj' not of fracture_geom class")
   if (! missing(type)) if (! is.character(type)) stop("'type' not a string")
   points = data.frame(
@@ -36,8 +36,8 @@ extract.tet.mesh = function(obj, type=c("top","bottom"), transform=function(poin
   add.simplexes( v1 = tr[,1],     v2 = tr[,2],      v3 = tr[,3],      tag = "top")
   add.simplexes( v1 = tr[,1]+k,   v2 = tr[,2]+k,    v3 = tr[,3]+k,    tag = "bottom")
   add.simplexes( v1 = tr[,1]+2*k, v2 = tr[,2]+2*k,  v3 = tr[,3]+2*k,  tag = "middle")
-  add.simplexes( v1 = ed[,1],     v2 = ed[,2],      v3 = ed[,2]+k,    tag = br)
-  add.simplexes( v1 = ed[,2]+k,   v2 = ed[,1]+k,    v3 = ed[,1],      tag = br)
+  add.simplexes( v1 = ed[,2],     v2 = ed[,2]+k,    v3 = ed[,1],      tag = br)
+  add.simplexes( v1 = ed[,1]+k,   v2 = ed[,1],      v3 = ed[,2]+k,    tag = br)
   tr = sortRows(tr)
   add.simplexes( v1 = tr[,1]+k,   v2 = tr[,1],      v3 = tr[,2],      v4 = tr[,3],     tag = "interior")
   add.simplexes( v1 = tr[,2]+k,   v2 = tr[,2],      v3 = tr[,3],      v4 = tr[,1]+k,   tag = "interior")
@@ -47,6 +47,20 @@ extract.tet.mesh = function(obj, type=c("top","bottom"), transform=function(poin
   if (! missing(type)) simplexes = simplexes[simplexes$tag %in% type, , drop=FALSE]
   selp = na.omit(unique(c(simplexes$v1,simplexes$v2,simplexes$v3,simplexes$v4)))
   map = rep(0,nrow(points)); map[selp] = seq_len(length(selp))
+  
+  if (!missing(genie.h)) {
+    cl = genieclust::gclust(points)
+    map = cutree(cl, h = genie.h)
+    points = aggregate(points, by=list(map), mean)
+    points$Group.1 = NULL
+    simplexes$v1 = map[simplexes$v1]
+    simplexes$v2 = map[simplexes$v2]
+    simplexes$v3 = map[simplexes$v3]
+    simplexes$v4 = map[simplexes$v4]
+  }
+  
+  dups = pmax(simplexes$v1 == simplexes$v2,simplexes$v1 == simplexes$v3,simplexes$v2 == simplexes$v3,simplexes$v1 == simplexes$v4,simplexes$v2 == simplexes$v4,simplexes$v3 == simplexes$v4,na.rm = TRUE)
+  simplexes = simplexes[dups == 0,]
   
   points = points[selp,,drop=FALSE]
   simplexes$v1 = map[simplexes$v1]
